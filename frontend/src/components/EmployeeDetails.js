5d969f27361a3beeeb3878d06e5cfb906af4a125
AnuragKashyap999 <161066252+AnuragKashyap999@users.noreply.github.com>
2025-05-22 23:06:37 +0530

    Update EmployeeDetails.js

--- frontend/src/components/EmployeeDetails.js
+++ frontend/src/components/EmployeeDetails.js
@@ -11,7 +11,7 @@ const EmployeeDetails = () => {
  
   useEffect(()=>{
       const fetch=async()=>{
-         const EmployeeDetail=await axios.get(`https://employees-cruv.onrender.com/api/v1/user/${id}`,{
+         const EmployeeDetail=await axios.get(`https://employee-zfam.onrender.com/api/v1/user/${id}`,{
           withCredentials:true,
          })
          console.log(EmployeeDetail.data.usersData);
